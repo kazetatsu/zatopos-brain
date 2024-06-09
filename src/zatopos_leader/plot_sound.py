64fed6587c3834418a942488dd0d1685b2ffa11c
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2024-06-09 14:15:58 +0900

    initial commit

---
+++ src/zatopos_leader/plot_sound.py
@@ -0,0 +1,35 @@
+import time
+import scipy.fft
+from worker_agent import SOUND_DEPTH, CH_NUM, WorkerAgentSerial
+import matplotlib.pyplot as plt
+import matplotlib.animation
+import numpy as np
+
+
+if __name__ == "__main__":
+    agent = WorkerAgentSerial("/dev/ttyACM0")
+
+    time.sleep(1.0)
+
+    fig = plt.figure()
+    x = np.arange(64)
+    y = 512 * np.ones((SOUND_DEPTH,), dtype=np.int16)
+
+    def update_func(frame, x, y):
+        plt.cla()
+        plt.ylim(0,1100)
+        y = agent.read_sound()
+        for ch in range(CH_NUM):
+            plt.plot(x, y[ch])
+
+    fanim = matplotlib.animation.FuncAnimation(
+        fig=fig,
+        func=update_func,
+        fargs=(x,y),
+        interval=200,
+        frames=range(32),
+        repeat=True
+    )
+
+    plt.show()
+    # fanim.save('sin.gif', writer='pillow')
