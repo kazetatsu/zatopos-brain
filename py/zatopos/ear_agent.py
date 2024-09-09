b0741981f2bef02110b7e6795c5b4ef5f2ae17bd
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2024-09-09 14:53:37 +0900

    Delete ear_agent using serial coomunication

--- py/zatopos/ear_agent.py
+++ py/zatopos/ear_agent.py
@@ -1,75 +1,18 @@
 import sys
 import numpy as np
-import serial
 import os
 import ctypes
 import subprocess
 
 from .sound import *
 
-DATA_BYTE = 4 # how many bytes per one value
-
 LIBZATOPOS_PATH = os.path.join(sys.prefix, "lib", "libzatopos.so")
 
-class EarAgentBase:
-    def __init__(self):
-        pass
-    def read_sound(self, dtype=np.int16) -> np.ndarray:
-        pass
-
-
-class EarAgentSerial(EarAgentBase, serial.Serial):
-    def __init__(self, port, baudrate=115200):
-        EarAgentBase.__init__(self)
-        serial.Serial.__init__(self, port=port, baudrate=baudrate)
-
-
-    def read_sound(self, length:int, dtype=np.int16) -> np.ndarray:
-        d = 4
-
-        x = np.zeros(shape=(NUM_MIC_CHS, length), dtype=np.int16)
-
-        i = 0
-        loop = 0
-        while i < length and loop < 1.5 * length:
-            sounds = self.readline().decode().strip().split(',')
-
-            error_msg = None
-
-            if len(sounds) != NUM_MIC_CHS:
-                error_msg = "too many/few words @ i={}".format(i)
-            else:
-                for ch in range(NUM_MIC_CHS):
-                    if len(sounds[ch]) != 3:
-                        error_msg = "too many/few letters @ i={} ch={}".format(i,ch)
-                        break
-
-                    try:
-                        x[ch, i] = int(sounds[ch], 16)
-                    except ValueError:
-                        error_msg = "data is not integer @ i={} ch={}".format(i, ch)
-                        break
-
-                    if x[ch, i] >= 1024 or x[ch, i] < 0:
-                        error_msg = "data is too big/small @ i={} ch={}".format(i, ch)
-                        break
-
-            if error_msg == None:
-                i += 1
-            else:
-                print(error_msg)
-                print(sounds)
-            loop += 1
-
-        return x.astype(np.int16)
-
-
-class EarAgentUSB(EarAgentBase):
+class EarAgent:
     def __init__(self, bus_no, dev_addr):
-        EarAgentBase.__init__(self)
         self.libzatopos = ctypes.cdll.LoadLibrary(LIBZATOPOS_PATH)
 
-        self.c_agent = ctypes.c_void_p(self.libzatopos.worker_agent_malloc())
+        self.c_agent = ctypes.c_void_p(self.libzatopos.ear_agent_malloc())
 
         ret = self.libzatopos.ear_agent_init(self.c_agent, bus_no, dev_addr)
 
@@ -96,7 +39,7 @@ class EarAgentUSB(EarAgentBase):
         return sound
 
 
-def get_ear_agent() -> EarAgentBase:
+def get_ear_agent() -> EarAgent:
     ret_lsusb = subprocess.run("lsusb | grep kazetatsu", shell=True, stdout=subprocess.PIPE)
     if ret_lsusb.stdout is None:
         raise Exception("subprocess error")
@@ -109,4 +52,4 @@ def get_ear_agent() -> EarAgentBase:
     cs = ss[0].split(' ')
     bus_no = int(cs[1])
     dev_addr = int(cs[3][0:3])
-    return EarAgentUSB(bus_no, dev_addr)
+    return EarAgent(bus_no, dev_addr)
