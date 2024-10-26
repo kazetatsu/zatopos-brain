614b35ed3e04412a48a346647d76589fb8ef38e5
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2024-10-27 00:14:23 +0900

    Update sample

--- samples/plot_sound.py
+++ samples/plot_sound.py
@@ -1,39 +1,31 @@
 import time
 import datetime
-from zatopos import EAR_NUM_MICS, EAR_WINDOW_LEN, get_ear_agent
+from zatopos import EAR_NUM_MICS, EAR_WINDOW_LEN, get_ear_driver
 import matplotlib.pyplot as plt
 import matplotlib.animation
 import numpy as np
 
-READ_TIMES = 5
-SOUND_LEN = READ_TIMES * EAR_WINDOW_LEN
+RECEIVE_TIMES = 3
+SOUND_LEN = RECEIVE_TIMES * EAR_WINDOW_LEN
 
 
 if __name__ == "__main__":
-    agent = get_ear_agent()
+    driver = get_ear_driver()
 
     time.sleep(1.0)
 
     fig = plt.figure()
     x = np.arange(SOUND_LEN)
-    y = np.ndarray((EAR_NUM_MICS, SOUND_LEN), dtype=np.int16)
+    y = np.ndarray((SOUND_LEN, EAR_NUM_MICS), dtype=np.int16)
 
     def update_func(frame, x, y):
         plt.cla()
         plt.ylim(0,1100)
 
-        s = datetime.datetime.now()
-        for i in range(READ_TIMES):
-            y[:, i * EAR_WINDOW_LEN : (i+1) * EAR_WINDOW_LEN] = agent.read_sound()
-            while True:
-                time.sleep(0.005)
-                e = datetime.datetime.now()
-                if (e-s).microseconds > 32000:
-                    s = e
-                    break
+        driver.receive(y)
 
         for ch in range(EAR_NUM_MICS):
-            plt.plot(x, y[ch])
+            plt.plot(x, y[:,ch])
 
     fanim = matplotlib.animation.FuncAnimation(
         fig=fig,
