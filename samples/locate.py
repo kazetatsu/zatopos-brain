427665477dbe10df81af93afc0ea34b781794886
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2024-10-27 00:27:48 +0900

    Show result which has good S/N ratio

--- samples/locate.py
+++ samples/locate.py
@@ -11,20 +11,25 @@ if __name__ == "__main__":
     x.append(np.loadtxt("data/sound002.csv", dtype=np.float32))
     x = np.array(x)
 
-    freq_i = np.arange(16,24, dtype=np.uint8)
+    eigval, eigvec = zatopos.fft_eig(x, min_freq_index=16, max_freq_index=25)
+    print(eigvec)
 
-    sss = zatopos.get_signal_spaces(x, freq_filter=freq_i)
-    print(sss)
+    result = np.zeros((10,8,8),dtype=np.float32)
 
-    locator = zatopos.Locator(freq_filter=freq_i)
+    music = zatopos.Musical(min_freq_index=16, max_freq_index=25)
 
     s = datetime.datetime.now()
-    res = locator.locate(sss)
+    music.search(eigvec, result)
     e = datetime.datetime.now()
     print(e-s)
 
-    print(res)
+    sn_ratio = eigval[:,0] / eigval.sum(axis=1)
+    sn_max_if = np.argmax(sn_ratio) # Frequency index which has max S/N(signal noise) ratio
+    print("S/N max")
+    print(sn_max_if)
+    print("result as text")
+    print(result[sn_max_if])
 
     plt.figure()
-    plt.imshow(res)
+    plt.imshow(result[sn_max_if])
     plt.show()
